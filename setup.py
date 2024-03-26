5558543e685bfce21e56abbae87e52337fadf474
Nick Janne <njanne@umich.edu>
2024-03-26 19:18:34 -0400

    Added nn.module blocks for everything up to ActionSampler

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup (
+    name='deep_rob_term_project_w24',
+    version='0.1',
+    packages=find_packages(),
+    package_dir={'torchdpf': 'torchdpf'},
+)
---
+++ torchdpf/models/initializations.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+def init_kaiming_normal(self, m):
+    if type(m) == nn.Linear or type(m) == nn.Conv2d:
+        torch.nn.init.kaiming_normal_(m.weight)
+        torch.nn.init.zeros_(m.bias)
+        
+def init_xavier_normal(self, m):
+    if type(m) == nn.Linear or type(m) == nn.Conv2d:
+        torch.nn.init.xavier_normal_(m.weight)
+        torch.nn.init.zeros_(m.bias)
